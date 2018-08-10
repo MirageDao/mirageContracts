06753436b717439955793472426033f5e44765c6
Zac Mitton <zacmitton22@gmail.com>
2018-08-10 00:24:58 -0400

    after brainstorm with tjaden

--- contracts/ShieldedToken.sol
+++ contracts/ShieldedToken.sol
@@ -1,32 +1,47 @@
 pragma solidity 0.4.24;
 
 import "zeppelin-solidity/contracts/token/ERC827/ERC827Token.sol";
-import "./DevDao.sol";
+// import "./Collective.sol";
+import "./GnosisWallet.sol";
 import "./Bancor.sol";
 
 //tokens and ether expressed in elementary particles unless specified otherwise
 contract MirageToken is ERC827Token{
 
-  DevDao public devDao;
+  address public collective;
   Bancor public bancor;
   uint   public createdAt;
   uint   public totalSupply;
-  uint   public E18 = 1000000000000000000;
+  uint   public constant E18 = 1000000000000000000;
 
-  constructor() public {
+  constructor(address _collective) public {
     createdAt = now; // or 1518567207 Feb 14 2018: Date of first meeting
-    devDao = new DevDao(address(this));
+    collective = _collective;
     bancor = new Bancor();
   }
 
-  function updateDevTokens() public {
-    uint newSupply = momentarySupply();
-    balances[devDao] += newSupply - totalSupply;
-    totalSupply = newSupply;
+  modifier onlyCollective(){ if (msg.sender == collective) _; }
+
+  function changeCollective(address _collective) onlyCollective { collective = _collective; }
+  function changeShieldLogic(address _shieldLogic) onlyCollective { shieldLogic = _shieldLogic; }
+
+  function releaseTokens() public onlyCollective {
+    uint newSupply = momentarySupply() - totalSupply;
+    totalSupply          += newSupply;
+    balances[collective] += newSupply;
+  }
+
+  address shieldLogic;
+  function shield(uint value, bytes data) payable{
+    approve(value, shieldLogic)//check order
+    shieldLogic.call.value(msg.value)(data)
+  }
+
+  function unshield(bytes data){
+    shieldLogic.call.value(msg.value)(data)
   }
 
   function momentarySupply() view public returns(uint){
-    // 67167592423 67167592054
     (uint256 estimate, uint256 precision) = bancor.power(67167592423, 67167592054, uint32(now - createdAt), 1);
     // "2**precision / estimate" is reciprocal which we want because we wanted the 
     // result of the negative exponent all along.
@@ -37,7 +52,37 @@ contract MirageToken is ERC827Token{
   }
 }
 
+contract shieldLogic
+  MirageToken mirage;
+  Collective collective;
+  modifier onlyCollective(){ if (msg.sender == collective) _; }
 
+  // function () {
+
+  // }
+
+  function shield(bytes data) payable{
+    revert("not yet available");
+  }
+
+  function unshield(bytes data) payable{
+    revert;
+  }
+
+  // function shield(bytes data) payable{
+  //   mirage.transferFrom(from, address(this), value);
+  // }
+
+  // function unshield(bytes data) payable{
+  //   if(proofIsCorrect){
+  //     mirage.transfer(to, value)
+  //   }
+  // }
+
+  // function upgrade() onlyCollective{
+  //   tran
+  // }
+  // this version doesnt need upgrade path for transfering deposits, because it doesnt take any
 
 
 
